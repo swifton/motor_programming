f31126e5a19ff3f1d237831196ef16be4df7527f
Anton Strizhov <anton.strizhov@gmail.com>
2013-03-26 22:55:50 -0700

    Program input

--- motor.js
+++ motor.js
@@ -4,6 +4,20 @@ function draw() {
    
 }
 
+function startProgram() {
+   var select;
+   var option;
+   for (var i = 0; i < 7; i++) {
+      select = document.getElementById(String(i));
+      option = select.options[select.selectedIndex].value;
+      program[i] = option;
+   }
+}
+
+function stopProgram() {
+
+}
+
 function GameLoop() {
    clear();
    draw();
@@ -11,6 +25,35 @@ function GameLoop() {
    gLoop = setTimeout(GameLoop, 1000 / 200);
 }
 
-GameLoop();
+var program = new Array(7);
 
-//c.addEventListener('mousedown', clickReporter, false);
+function init() {
+   var select;
+   var option;
+   var options = ["Wait", "Move", "Turn Left", "Turn Right", "Pick Up"];
+
+   for (var i = 0; i < 7; i++) {
+      select = document.createElement("select");
+      select.setAttribute("id", String(i));
+   
+      for (o in options) {
+         option = document.createElement("option");
+         option.value = options[o];
+         option.text = options[o];
+         select.appendChild(option);
+      }
+      console.log(select);
+      document.getElementById("program").appendChild(select);
+   }
+}
+
+init();
+//GameLoop();
+
+//select.style.width = "300px";
+
+//option.text = "Kiwi";console.log(option);
+
+//x.add(option,null);console.log(x);
+
+//document.getElementById("program").appendChild(select);
